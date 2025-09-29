b4604910146b0b0eaf254ea3973e3f6299176510
Dong Qianbo <pencilate@outlook.com>
2025-09-29 23:17:28 +0800

    Generate and add embeddings to term

--- service/openrouter_svc.py
+++ service/openrouter_svc.py
@@ -1,7 +1,10 @@
 import os
 import time
+
+import pydantic_core
 from dotenv import load_dotenv
 from fastapi import HTTPException
+from openai import AsyncOpenAI
 from pydantic import BaseModel
 from pydantic_ai import Agent, ModelHTTPError, PromptedOutput
 from pydantic_ai.models.openai import OpenAIChatModel
@@ -15,6 +18,10 @@ DATA_EXTRACTION_SYSTEM_PROMPT = "You are a highly skilled data extraction specia
 class ExtractionOutput(BaseModel):
     name: str
     description: str
+
+
+class ExtractionOutputWEmbed(ExtractionOutput):
+    embedding: str
 
 
 llm_model = OpenAIChatModel(
@@ -31,10 +38,26 @@ data_extraction_agent = Agent(
     output_type=PromptedOutput(list[ExtractionOutput]),
 )
 
+embedding_client = AsyncOpenAI(
+    base_url=os.getenv("OPENAI_COMPAT_EMBED_API_ENDPOINT"),
+    api_key=os.getenv("OPENAI_COMPAT_EMBED_API_KEY"),
+)
+
+
+async def _embed_content(content: str) -> str:
+    print("Embedding content")
+    embedding = await embedding_client.embeddings.create(
+        input=content,
+        model=os.getenv("EMBED_MODEL_NAME"),
+    )
+    embedding_data = embedding.data[0].embedding
+    embedding_json = pydantic_core.to_json(embedding_data).decode()
+    return embedding_json
+
 
 async def _extract_paper_content(
     prompt: str, error_message: str
-) -> list[ExtractionOutput]:
+) -> list[ExtractionOutputWEmbed]:
     time_start = time.perf_counter()
 
     try:
@@ -44,10 +67,21 @@ async def _extract_paper_content(
 
     time_end = time.perf_counter()
     print(f"Completed in {time_end - time_start:.2f} seconds")
-    return result.output
+
+    extracted_term = result.output
+    extracted_term_with_embed = []
+    for term in extracted_term:
+        term_embed = await _embed_content(term.name)
+        extracted_term_with_embed.append(
+            ExtractionOutputWEmbed(
+                name=term.name, description=term.description, embedding=term_embed
+            )
+        )
+
+    return extracted_term_with_embed
 
 
-async def extract_paper_dataset(paper_text: str) -> list[ExtractionOutput]:
+async def extract_paper_dataset(paper_text: str) -> list[ExtractionOutputWEmbed]:
     print("Extracting datasets")
     prompt = f"Given the following academic paper text:\n\n{paper_text}\n\nExtract datasets and benchmarks used for training or evaluation in the paper."
     return await _extract_paper_content(
@@ -55,7 +89,7 @@ async def extract_paper_dataset(paper_text: str) -> list[ExtractionOutput]:
     )
 
 
-async def extract_paper_models(paper_text: str) -> list[ExtractionOutput]:
+async def extract_paper_models(paper_text: str) -> list[ExtractionOutputWEmbed]:
     print("Extracting models")
     prompt = f"Given the following academic paper text:\n\n{paper_text}\n\nExtract the models referenced, such as language models, rerank models, embed models, models that implements a technique or models used for comparison, in the paper. Exclude methods, benchmarks and framework."
     return await _extract_paper_content(
@@ -63,7 +97,7 @@ async def extract_paper_models(paper_text: str) -> list[ExtractionOutput]:
     )
 
 
-async def extract_paper_methods(paper_text: str) -> list[ExtractionOutput]:
+async def extract_paper_methods(paper_text: str) -> list[ExtractionOutputWEmbed]:
     print("Extracting methods")
     prompt = f"Given the following academic paper text:\n\n{paper_text}\n\nExtract the terms of techniques used in the paper. Exclude language models, rerank models, embed models."
     return await _extract_paper_content(
@@ -71,7 +105,7 @@ async def extract_paper_methods(paper_text: str) -> list[ExtractionOutput]:
     )
 
 
-async def extract_paper_tasking(paper_text: str) -> list[ExtractionOutput]:
+async def extract_paper_tasking(paper_text: str) -> list[ExtractionOutputWEmbed]:
     print("Extracting tasking")
     prompt = f"Given the following academic paper text:\n\n{paper_text}\n\nExtract the tasks/use cases covered in the paper"
     return await _extract_paper_content(
